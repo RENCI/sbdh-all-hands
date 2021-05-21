83f76300e2390ed747eda4831f22615b4929d100
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-05-21 18:11:10 -0400

    embed registration form; remove broken css

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,8 +1,12 @@
+import { Container, Loader } from "semantic-ui-react";
 
 function Registration() {
+    const Loading = <Loader content='Loading...' />
     return (
-        <div>
-            <p>This is where the registration content is going</p>
+        <div className='pageContain'>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfM0Jqz1nSg-tW29oV7cRjmkojffhXjFmhp5-Ds6ud5zPaWvg/viewform?embedded=true" title='Registration form' height="1000" width="100%" frameborder="0" marginheight="0" marginwidth="0">
+                {Loading}
+            </iframe>
         </div>
     )
 }
