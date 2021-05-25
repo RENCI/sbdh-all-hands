e9585334d7b09e0969dc9b7b8158b04d40b53c14
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-05-25 17:43:14 -0400

    add active link logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import { Grid } from 'semantic-ui-react'
-import { Router as ReachRouter } from '@reach/router'
+import { LocationProvider, Router as ReachRouter } from '@reach/router'
 
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
@@ -12,25 +12,26 @@ import Speaker from './components/Speaker';
 import PageNotFound from './components/404';
 
 function App() {
-
   return (
-    <div className="App">
-      <div className='wrapper'>
-        <NavBar />
-        <Grid centered columns={3} className='pageBody' padded>
-          <Grid.Column width={12}>
-            <ReachRouter primary={false}>
-                <Home path='/' />
-                <Agenda path='/event-agenda' />
-                <Registration path='/register' />
-                <Speaker path='/speakers' />
-                <PageNotFound default />
-            </ReachRouter>
-          </Grid.Column>
-        </Grid>
-        <Footer />
+    <LocationProvider>
+      <div className="App">
+        <div className='wrapper'>
+          <NavBar />
+          <Grid centered columns={3} className='pageBody' padded>
+            <Grid.Column width={12}>
+              <ReachRouter primary={false}>
+                  <Home path='/' />
+                  <Agenda path='/event-agenda' />
+                  <Registration path='/register' />
+                  <Speaker path='/speakers' />
+                  <PageNotFound default />
+              </ReachRouter>
+            </Grid.Column>
+          </Grid>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </LocationProvider>
   );
 }
 
