2177c65350010914b512043d1f522ef880a34dae
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-05-26 17:30:05 -0400

    add example speaker card

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,8 +1,18 @@
+import { Card, Header } from "semantic-ui-react";
+
+import placeholder from './images/home/300x300_placeholder.png'
 
 function Speaker() {
     return (
-        <div>
-            <p>This is where the speaker content is going</p>
+        <div className="page-contain">
+            <Header as='h1' className="page-title" textAlign='center' content="Speakers" subheader="Summary about speakers and how we're so excited. Probably the blurb from the home page." />
+            <Card
+                image={placeholder}
+                header='Elliot Baker'
+                meta='Sound Engineer, RENCI'
+                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra="tags"
+            />
         </div>
     )
 }
