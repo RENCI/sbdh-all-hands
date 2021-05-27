9fdefe3c40bf93f3b81d71875309a1136f442cb6
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-05-27 15:59:17 -0400

    change footer links to sbdh

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,8 +17,7 @@ function Footer() {
                                     <Menu.Item header as={Link} to='/' content='Home' className='footer-link' />
                                     <Menu.Item as={Link} to='/register' content='Registration' className='footer-link'/>
                                     {/* <Menu.Item as={Link} to='/event-agenda' content='Agenda' className='footer-link'/>
-                                    <Menu.Item as={Link} to='/speakers' content='Speakers' className='footer-link'/>
-                                    <Menu.Item href='https://southbigdatahub.org/' target='_blank' noopener content='South Hub Main Site' className='footer-link' /> */}
+                                    <Menu.Item as={Link} to='/speakers' content='Speakers' className='footer-link'/> */}
                                 </Menu>
                             </Grid.Column>
                             <Grid.Column width={8} padded>
@@ -27,9 +26,9 @@ function Footer() {
                                 </Grid.Row>
                                 <Grid.Row id='socials'>
                                     <List horizontal>
-                                        <List.Item>
+                                        {/* <List.Item>
                                             <Button className='social-btn' icon='computer' color='grey' href='https://southbigdatahub.org/' target='_blank' noopener title="South Hub Main Site" />
-                                        </List.Item>
+                                        </List.Item> */}
                                         <List.Item>
                                             <Button className='social-btn' icon='twitter' color='twitter' href='https://twitter.com/SouthBigDataHub' target='_blank' noopener title="South Hub Twitter" />
                                         </List.Item>
@@ -45,13 +44,13 @@ function Footer() {
                         </Grid.Row>
                         <Grid.Row  textAlign="left">
                             <Grid.Column width={2} verticalAlign='middle'>
-                                <Image size='tiny' src={logo} alt='logo' />
+                                <Image size='tiny' src={logo} alt='logo' as={Link} to="https://southbigdatahub.org/" noopener target="_blank" />
                             </Grid.Column>
                             <Grid.Column width={9}>
                                 <Grid.Row>
                                     <Menu text inverted stackable fluid>
                                         <Menu.Item as={Link} name="Code of Conduct" to="/code-of-conduct" className='footer-extra' />
-                                        {/* <Menu.Item as={Link} name="Sponsor or Booth Host Interest" to="#" className='footer-extra' /> */}
+                                        <Menu.Item as={Link} name="Learn About The South Hub" to="https://southbigdatahub.org/" noopener target="_blank" className='footer-extra' />
                                     </Menu>
                                 </Grid.Row>
                                 <Grid.Row>
