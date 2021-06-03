24021e243faeff1407a3ad56b355823b21db8e67
Matt Watson <mattwat@gmail.com>
2021-06-03 13:10:24 -0400

    fix mobile overflow/rearrange footer contents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,66 +5,53 @@ import { Link } from '@reach/router';
 import logo from './images/logos/SBDH-logo-white-vertical.png'
 import logoNSF from './images/logos/nsf.png'
 
+const { Column, Row } = Grid
+
 function Footer() {
     return(
         <footer className='page-footer'>
             <Segment inverted vertical style={{ marginTop: '3rem', padding: '2rem 0rem' }}>
                 <Container>
-                    <Grid stackable>
-                        <Grid.Row>
-                            <Grid.Column width={9}>
-                                <Menu text >
-                                    <Menu.Item header as={Link} to='/' content='Home' className='footer-link' />
-                                    <Menu.Item as={Link} to='/register' content='Registration' className='footer-link'/>
-                                    {/* <Menu.Item as={Link} to='/event-agenda' content='Agenda' className='footer-link'/>
-                                    <Menu.Item as={Link} to='/speakers' content='Speakers' className='footer-link'/> */}
-                                    <Menu.Item as={Link} to='/get-involved' content='Get Involved' className="footer-link" />
-                                    <Menu.Item as={Link} to='/register' position='right'>
-                                        <Button color='orange' size='small' content='REGISTER NOW' />
-                                    </Menu.Item>
-
-                                </Menu>
-                            </Grid.Column>
-                            <Grid.Column width={7} padded>
-                                <Grid.Row>
-                                    <Header as='h3' style={{color: "#f37521"}} content='Follow us online!' />
-                                </Grid.Row>
-                                <Grid.Row id='socials'>
-                                    <List horizontal>
-                                        {/* <List.Item>
-                                            <Button className='social-btn' icon='computer' color='grey' href='https://southbigdatahub.org/' target='_blank' noopener title="South Hub Main Site" />
-                                        </List.Item> */}
-                                        <List.Item>
-                                            <Button className='social-btn' icon='twitter' color='twitter' href='https://twitter.com/SouthBigDataHub' target='_blank' noopener title="South Hub Twitter" />
-                                        </List.Item>
-                                        <List.Item>
-                                            <Button className='social-btn' icon='youtube' color='youtube' href='https://www.youtube.com/channel/UCxmSez9cqyxJ6jQhs9dRkeQ' target='_blank' noopener title="South Hub YouTube" />
-                                        </List.Item>
-                                        {/* <List.Item>
-                                            <Button className='social-btn' icon='linkedin' color='linkedin' href='#' target='_blank' noopener title="South Hub YouTube" />
-                                        </List.Item> */}
-                                    </List>
-                                </Grid.Row>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row  textAlign="left">
-                            <Grid.Column width={2} verticalAlign='middle'>
-                                <Image size='tiny' src={logo} alt='logo' href="https://southbigdatahub.org/" rel="noopener" target="_blank" />
-                            </Grid.Column>
-                            <Grid.Column width={9}>
-                                <Grid.Row>
-                                    <Menu text inverted stackable fluid>
-                                        <Menu.Item as={Link} name="Code of Conduct" to="/code-of-conduct" className='footer-extra' />
-                                        <Menu.Item name="Learn About The South Hub" href="https://southbigdatahub.org/about" rel="noopener" target="_blank" className='footer-extra' />
-                                    </Menu>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Image size='tiny' src={logoNSF} floated="left" />
-                                    The South Big Data Innovation Hub is supported by the National Science Foundation through awards: #1550305 and 1916589
-                                </Grid.Row>
-                            </Grid.Column>
-                        </Grid.Row>
+                    <Grid inverted stackable>
+                        <Row>
+                            <Column mobile={16} tablet={5} computer={6}>
+                                <List link inverted horizontal>
+                                    <List.Item header as={Link} to='/' content='Home' className='footer-link' />
+                                    <List.Item as={Link} to='/register' content='Registration' className='footer-link'/>
+                                    <List.Item as={Link} to='/get-involved' content='Get Involved' className="footer-link" />
+                                </List>
+                                <List text inverted>
+                                    <Menu.Item as={Link} name="Code of Conduct" to="/code-of-conduct" className='footer-extra' />
+                                    <Menu.Item name="Learn About The South Hub" href="https://southbigdatahub.org/about" rel="noopener" target="_blank" className='footer-extra' />
+                                </List>
+                            </Column>
+                            <Column mobile={16} tablet={5} computer={4} className="footer-register-button-container" verticalAlign="middle">
+                                <Button color='orange' content='REGISTER NOW' as={ Link } to="/register" />
+                            </Column>
+                            <Column mobile={16} tablet={5} computer={6}>
+                                <Header as='h3' style={{color: "#f37521"}} content='Follow us online!' />
+                                <List horizontal>
+                                    <List.Item>
+                                        <Button className='social-btn' icon='twitter' color='twitter' href='https://twitter.com/SouthBigDataHub' target='_blank' noopener title="South Hub Twitter" />
+                                    </List.Item>
+                                    <List.Item>
+                                        <Button className='social-btn' icon='youtube' color='youtube' href='https://www.youtube.com/channel/UCxmSez9cqyxJ6jQhs9dRkeQ' target='_blank' noopener title="South Hub YouTube" />
+                                    </List.Item>
+                                </List>
+                            </Column>
+                        </Row>
                     </Grid>
+                    
+                    <br/>
+                    <div class="ui divider"></div>
+                    <br/>
+                    
+                    <div className="footer-nsf-acknwledgment">
+                        <Image size='tiny' src={logoNSF} className="footer-nsf-acknwledgment__logo"/>
+                        <div className="footer-nsf-acknwledgment__blurb">
+                            The South Big Data Innovation Hub is supported by the National Science Foundation through awards: #1550305 and 1916589
+                        </div>
+                    </div>
                 </Container>
             </Segment>
         </footer>
