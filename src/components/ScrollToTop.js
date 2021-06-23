12d0ba712976ef128ddc04dbcb0504ba6b8cce7d
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-06-23 16:22:17 -0400

    add smooth scrolling to top

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 export const ScrollToTop = ({ children, location }) => {
-  React.useEffect(() => window.scrollTo(0, 0), [location.pathname])
+  React.useEffect(() => window.scrollTo({top: 0, left: 0, behavior: 'smooth'}), [location.pathname])
   return children
 }
