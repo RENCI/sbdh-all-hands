7ae079759ccad9190c6afd7fe999fa142344c08c
Matt Watson <mattwat@gmail.com>
2021-06-23 15:10:48 -0400

    add scroll-top-top component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Participate from './components/Participate';
 import Conduct from './components/ConductCode';
 import PageNotFound from './components/404';
 import SpeakerInfo from './components/SpeakerInfo';
-
+import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
 
@@ -25,6 +25,7 @@ function App() {
           <Grid centered columns={3} className='pageBody' padded>
             <Grid.Column width={14} className="page-content">
               <ReachRouter primary={false}>
+                <ScrollToTop path="/">
                   <Home path='/' />
                   <Agenda path='/event-agenda' />
                   <Registration path='/register' />
@@ -33,6 +34,7 @@ function App() {
                   <Participate path='/get-involved' />
                   <Conduct path="/code-of-conduct" />
                   <PageNotFound default />
+                </ScrollToTop>
               </ReachRouter>
             </Grid.Column>
           </Grid>
