df5da7a5521463e5f10118b8ea81f38bf1da3284
Matt Watson <mwatson@renci.org>
2024-02-06 16:38:57 -0500

    tidy layout

--- src/views/home.js
+++ src/views/home.js
@@ -5,20 +5,30 @@ import { Markdown } from '../components/markdown'
 import flyer from '../images/2024-04-18_save-the-date_partnership-forum.png'
 
 export const HomeView = () => {
-
   return (
     <Stack
       direction={{ sm: 'column', md: 'row' }}
+      justifyContent="flex-start"
+      gap={{ sm: 0, md: 4 }}
       sx={{
-        '.flyer-preview': {
+        '.flyer-container': {
+          borderRadius: 12,
+          mt: 2,
           flex: '1 0 400px',
-          p: 4,
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          '.flyer': {
+            borderRadius: 12,
+            border: '2px solid var(--color-sbdh)',
+            verticalAlign: 'bottom',
+          },
+          'svg': {
+            display: 'none'
+          },
         },
         '.description': {
-          flex: '1 1 700px',
+          flex: { sm: '1', md: '1 1 700px'},
           textAlign: 'center',
           display: 'flex',
           flexDirection: 'column',
@@ -27,13 +37,14 @@ export const HomeView = () => {
         },
       }}
     >
-      <Box className="flyer-preview">
+      <Box className="flyer-container">
         <Link to={ flyer }>
           <img
+            className="flyer"
             src={ flyer }
+            alt="Click for large, high-resolution image"
             width="100%"
-          />
-        </Link>
+          /></Link>
       </Box>
 
       <Box className="description">
