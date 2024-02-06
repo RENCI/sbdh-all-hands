09e2023bc02e585c079d4ea4956f8daaab3f9089
Matt Watson <mwatson@renci.org>
2024-02-06 14:18:43 -0500

    add flyer to landing page
    install mui joy

--- src/views/home.js
+++ src/views/home.js
@@ -1,8 +1,44 @@
 import content from '../content/home.md'
+import { Box, Stack } from '@mui/joy'
+import { Link } from '../components/link'
 import { Markdown } from '../components/markdown'
+import flyer from '../images/2024-04-18_save-the-date_partnership-forum.png'
 
 export const HomeView = () => {
+
   return (
-    <Markdown>{ content }</Markdown>
+    <Stack
+      direction={{ sm: 'column', md: 'row' }}
+      sx={{
+        '.flyer-preview': {
+          flex: '1 0 400px',
+          p: 4,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        '.description': {
+          flex: '1 1 700px',
+          textAlign: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+      }}
+    >
+      <Box className="flyer-preview">
+        <Link to={ flyer }>
+          <img
+            src={ flyer }
+            width="100%"
+          />
+        </Link>
+      </Box>
+
+      <Box className="description">
+        <Markdown>{ content }</Markdown>
+      </Box>
+    </Stack>
   )
 }
