694d18047077b14d1c81ff8dfb877ca131e6e554
suejinkim20 <kim.suejin.20@gmail.com>
2024-02-20 17:02:21 -0500

    hard-code buttons for registration and agenda

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,5 @@
 import content from '../content/home.md'
-import { Box, Stack } from '@mui/joy'
+import { Box, Stack, Button } from '@mui/joy'
 import { Link } from '../components/link'
 import { Markdown } from '../components/markdown'
 import flyer from '../images/2024-04-18_save-the-date_partnership-forum.png'
@@ -29,11 +29,14 @@ export const HomeView = () => {
         },
         '.description': {
           flex: { sm: '1', md: '1 1 700px'},
-          textAlign: 'center',
+          textAlign: 'left',
           display: 'flex',
           flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          justifyContent: 'flex-start',
+          // alignItems: 'center',
+          'h1': {
+            textAlign: 'center',
+          }
         },
       }}
     >
@@ -47,8 +50,16 @@ export const HomeView = () => {
           /></Link>
       </Box>
 
-      <Box className="description">
+      <Box className="description" >
         <Markdown>{ content }</Markdown>
+        <Stack direction="row" gap={4} justifyContent="center" mt={2}>
+          <Link button size="lg" to="https://docs.google.com/forms/d/e/1FAIpQLSdCXJLul-2ZShjKi2qHyQdMSPz3VPPJsHbuzgA0jSx4zDGfJQ/viewform">
+              Register Here
+          </Link>
+          <Link button size="lg" to="https://docs.google.com/document/d/1RC1de1Tj-u_9cUONHUihtyWXy8MQq20AHMYhUGyNNX4/edit?usp=sharing">
+              View the Agenda
+          </Link>
+        </Stack>
       </Box>
     </Stack>
   )
