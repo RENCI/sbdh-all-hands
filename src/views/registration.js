caf6eab2c2a7408c3d269031d9d4d18f0bebccd6
suejinkim20 <kim.suejin.20@gmail.com>
2024-02-26 13:42:12 -0500

    add markdown title and text to registration page

--- src/views/registration.js
+++ src/views/registration.js
@@ -1,18 +1,20 @@
-import content from '../content/home.md'
-import { Box, Stack, Button } from '@mui/joy'
-import { Link } from '../components/link'
+import content from '../content/registration.md'
+import { Fragment } from 'react'
+import { Box } from '@mui/joy'
 import { Markdown } from '../components/markdown'
 
 export const RegistrationView = () => {
   return (
-    <Box>
-      <iframe 
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdCXJLul-2ZShjKi2qHyQdMSPz3VPPJsHbuzgA0jSx4zDGfJQ/viewform?embedded=true"
-        title='Registration form' 
-        height="1000" 
-        width="100%" 
-      >
-      </iframe>
-    </Box>
+    <Fragment>
+      <Markdown>{content}</Markdown>
+      <Box sx={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '1rem'}}>
+        <iframe 
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdCXJLul-2ZShjKi2qHyQdMSPz3VPPJsHbuzgA0jSx4zDGfJQ/viewform?embedded=true"
+          title='Registration form' 
+          height="3950" 
+          width="750px"
+        />
+      </Box>
+    </Fragment>
   )
 }
