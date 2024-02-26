32c1a00bf9fb837f3f57499faf53082083779f7a
suejinkim20 <kim.suejin.20@gmail.com>
2024-02-26 16:27:53 -0500

    shift colors in palette to darker shades

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -1,19 +1,21 @@
 export const palette = {
   primary: {
     50: '#fde7d8',
-    100: '#fcdbc5',
-    200: '#fbcfb1',
-    300: '#fac39e',
-    400: '#f9b78b',
-    500: '#f8ab77',
-    600: '#f79f64',
-    700: '#f69351',
-    800: '#f5873d',
-    900: '#f37521',
+    100: '#fbcfb1',
+    200: '#f9b78b',
+    300: '#f79f64',
+    400: '#f5873d',
+    500: '#f37521',
+    600: '#f36f16',
+    700: '#e9640c',
+    800: '#d55c0b',
+    900: '#c2540a',
 
     main: '#f37521',
     light: '#f8ab77',
     dark: '#e9640c',
   },
+  solidBg: 'var(--sbdh-palette-primary-500)',
+  solidActiveBg: 'var(--sbdh-palette-primary-500)',
 
 }
