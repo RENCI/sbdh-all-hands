7a0a76261b0e0f6748c24aa429ebe386f7b99488
suejinkim20 <kim.suejin.20@gmail.com>
2024-03-06 12:52:29 -0500

    update height and style of embedded google form iframe

--- src/views/registration.js
+++ src/views/registration.js
@@ -1,25 +1,27 @@
 import content from '../content/registration.md'
 import { Fragment } from 'react'
-import { Box } from '@mui/joy'
+import { Box, Card } from '@mui/joy'
 import { Markdown } from '../components/markdown'
 
 export const RegistrationView = () => {
   return (
     <Fragment>
       <Markdown>{content}</Markdown>
-      <Box 
-        component="iframe"
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdCXJLul-2ZShjKi2qHyQdMSPz3VPPJsHbuzgA0jSx4zDGfJQ/viewform?embedded=true"
-        title='Registration form' 
-        sx={{
-          width: '100%', 
-          display: 'flex', 
-          justifyContent: 'center', 
-          marginTop: '1rem', 
-          border: '0',
-          height: '4250px'
-        }}>
-      </Box>
+      <Card variant="outlined" >
+        <Box 
+          component="iframe"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdCXJLul-2ZShjKi2qHyQdMSPz3VPPJsHbuzgA0jSx4zDGfJQ/viewform?embedded=true"
+          title='Registration form' 
+          sx={{
+            width: '100%', 
+            display: 'flex', 
+            justifyContent: 'center', 
+            marginTop: '1rem', 
+            border: '0',
+            height: '50vh',
+          }}>
+        </Box>
+      </Card>
     </Fragment>
   )
 }
