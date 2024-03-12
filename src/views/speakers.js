f8f0594df7360788723e928239fc929f4d5700f0
suejinkim20 <kim.suejin.20@gmail.com>
2024-03-12 10:40:56 -0400

    use yaml file to map over speakers

--- src/views/speakers.js
+++ src/views/speakers.js
@@ -1,9 +1,35 @@
-import { Box, Typography } from '@mui/joy'
+import speakers from '../content/speakers/index.yaml'
+import { Box, Card, Typography, Stack } from '@mui/joy'
+import { Markdown } from '../components/markdown'
 
 export const SpeakersView = () => {
   return (
     <Box>
       <Typography level="h2">Speakers</Typography>
+      {speakers.map((speaker)=> {
+        return (
+          <Card key={speaker.name} m={2}>
+            <Stack
+              direction={{ xs: 'column', sm: 'row' }}
+              spacing={3}
+            >
+              <Box
+              sx={{minWidth: '200px'}}
+              >
+                <img 
+                src={speaker.photo}
+                width="100%"
+              />
+              </Box>
+              
+              <Stack direction="column">
+                <Typography level="h3">{speaker.name}</Typography>
+                <Markdown>{speaker.bio}</Markdown>
+              </Stack>
+            </Stack>
+          </Card>
+        )
+      })}
     </Box>
   )
 }
