127d2db618e722ce02dca5daa8219e250f23b9b3
suejinkim20 <kim.suejin.20@gmail.com>
2024-03-13 13:22:51 -0400

    reconfigure speaker content

--- src/views/speakers.js
+++ src/views/speakers.js
@@ -1,5 +1,6 @@
-import speakers from '../content/speakers/index.yaml'
-import { Box, Card, Typography, Stack } from '@mui/joy'
+import { Fragment } from 'react'
+import { speakers } from '../content/speakers/index.js'
+import { Box, Card, Typography, Stack, AspectRatio, Divider } from '@mui/joy'
 import { Markdown } from '../components/markdown'
 
 export const SpeakersView = () => {
@@ -8,26 +9,28 @@ export const SpeakersView = () => {
       <Typography level="h2">Speakers</Typography>
       {speakers.map((speaker)=> {
         return (
-          <Card key={speaker.name} m={2}>
+          <Fragment key={speaker.name}>
+          <Card key={speaker.name} m={2} size="lg" variant="plain">
             <Stack
               direction={{ xs: 'column', sm: 'row' }}
               spacing={3}
             >
-              <Box
-              sx={{minWidth: '200px'}}
-              >
-                <img 
-                src={speaker.photo}
-                width="100%"
-              />
+              <Box>
+                <AspectRatio ratio={1}  sx={{ width: 300 }}>
+                  <img 
+                    src={speaker.image}
+                    width="100%"
+                  />
+                </AspectRatio>
               </Box>
-              
               <Stack direction="column">
-                <Typography level="h3">{speaker.name}</Typography>
+                <Typography level="h3" sx={{marginTop: 0}}>{speaker.name}</Typography>
                 <Markdown>{speaker.bio}</Markdown>
               </Stack>
             </Stack>
           </Card>
+          <Divider/>
+          </Fragment>
         )
       })}
     </Box>
