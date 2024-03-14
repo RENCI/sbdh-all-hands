71ed0e26b2e29cc1fe89db611447523606efdc60
suejinkim20 <kim.suejin.20@gmail.com>
2024-03-14 16:23:58 -0400

    update spacing and add id's for each speaker on speaker page

--- src/views/speakers.js
+++ src/views/speakers.js
@@ -6,30 +6,30 @@ import { Markdown } from '../components/markdown'
 export const SpeakersView = () => {
   return (
     <Box>
-      <Typography level="h2">Speakers</Typography>
+      <Typography level="h2" sx={{marginBottom: '0'}}>Speakers</Typography>
       {speakers.map((speaker)=> {
         return (
           <Fragment key={speaker.name}>
-          <Card key={speaker.name} m={2} size="lg" variant="plain">
-            <Stack
-              direction={{ xs: 'column', sm: 'row' }}
-              spacing={3}
-            >
-              <Box>
-                <AspectRatio ratio={1}  sx={{ width: 300 }}>
-                  <img 
-                    src={speaker.image}
-                    width="100%"
-                  />
-                </AspectRatio>
-              </Box>
-              <Stack direction="column">
-                <Typography level="h3" sx={{marginTop: 0}}>{speaker.name}</Typography>
-                <Markdown>{speaker.bio}</Markdown>
+            <Box id={speaker.slug} sx={{paddingBottom: '3rem'}}></Box>
+            <Card variant="soft" sx={{backgroundColor:'#ffffff00'}}>
+              <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                spacing={3}
+              >
+                <Box>
+                  <AspectRatio ratio={1}  sx={{ width: 300 }}>
+                    <img 
+                      src={speaker.image}
+                      width="100%"
+                    />
+                  </AspectRatio>
+                </Box>
+                <Stack direction="column">
+                  <Typography level="h3" sx={{marginTop: 0}} >{speaker.name}</Typography>
+                  <Markdown>{speaker.bio}</Markdown>
+                </Stack>
               </Stack>
-            </Stack>
-          </Card>
-          <Divider/>
+            </Card>
           </Fragment>
         )
       })}
