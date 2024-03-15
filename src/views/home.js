b3bd5c2bff6df3ff7ad578e33797adf87bb2bea9
suejinkim20 <kim.suejin.20@gmail.com>
2024-03-15 14:24:50 -0400

    update styles

--- src/views/home.js
+++ src/views/home.js
@@ -33,9 +33,6 @@ export const HomeView = () => {
               borderRadius: 8,
               verticalAlign: 'bottom',
             },
-            'svg': {
-              display: 'none'
-            },
           },
           '.description': {
             marginTop: '1rem'
@@ -44,7 +41,7 @@ export const HomeView = () => {
         }}
       >
         <Box className="flyer-container">
-          <Link to={ dateBanner }>
+          <Link to={ dateBanner } noIcon>
             <img
               className="flyer"
               src={ dateBanner }
@@ -70,9 +67,6 @@ export const HomeView = () => {
         display: 'flex', 
         justifyContent: 'center',
         margin: '1.2rem auto',
-        'svg': {
-          display: 'none'
-        },
         '.focused-session-image': {
           maxWidth: '100%',
           '@media screen and (max-width: 600px)': {
@@ -80,7 +74,7 @@ export const HomeView = () => {
           }
         },
       }}>
-        <Link to={focusedSessionsImg}>
+        <Link to={focusedSessionsImg} noIcon>
           <img
             className="focused-session-image"
             src={focusedSessionsImg}
@@ -108,9 +102,9 @@ export const HomeView = () => {
           >
             <AspectRatio ratio="1" sx={{
               borderRadius: '100%',
-              transition: 'transform 0.1s',
-              ':hover': {
-                  transform:'scale(1.02)'
+              ':hover img': {
+                transition: 'transform 0.1s',
+                transform:'scale(1.02)'
               }
             }}>
               <img 
