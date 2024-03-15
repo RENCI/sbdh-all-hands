a25ddbeabf3112cd21bde8d35728c53bf94180c2
suejinkim20 <kim.suejin.20@gmail.com>
2024-03-15 14:14:16 -0400

    add scroll to hash behavior

--- src/views/speakers.js
+++ src/views/speakers.js
@@ -1,16 +1,28 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import { speakers } from '../content/speakers/index.js'
 import { Box, Card, Typography, Stack, AspectRatio, Divider } from '@mui/joy'
 import { Markdown } from '../components/markdown'
 
 export const SpeakersView = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 })
+    if (!location.hash) {
+      return
+    }
+    const targetElement = document.getElementById(location.hash.slice(1))
+    targetElement.scrollIntoView({ behavior: 'smooth' })
+  }, [location.hash])
+  
   return (
     <Box>
       <Typography level="h2" sx={{marginBottom: '0'}}>Speakers</Typography>
       {speakers.map((speaker)=> {
         return (
           <Fragment key={speaker.name}>
-            <Box id={speaker.slug} sx={{paddingBottom: '3rem'}}></Box>
+            <Box id={speaker.slug} sx={{paddingBottom: '3rem'}} mt="-2rem"></Box>
             <Card variant="soft" sx={{backgroundColor:'#ffffff00'}}>
               <Stack
                 direction={{ xs: 'column', sm: 'row' }}
