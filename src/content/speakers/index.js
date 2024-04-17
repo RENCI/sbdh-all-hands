98e44788377b724cfd446bba9993c69f860f83d4
suejinkim20 <kim.suejin.20@gmail.com>
2024-04-17 16:50:20 -0400

    add speakers second round

--- src/content/speakers/index.js
+++ src/content/speakers/index.js
@@ -6,20 +6,34 @@ import jeminGeorgeImg from '../../images/jeminGeorge.jpg'
 import jeminGeorge from './jeminGeorge.yaml'
 import anikaGossImg from '../../images/anikaGoss.jpg'
 import anikaGoss from './anikaGoss.yaml'
+import debbyCovingtonImg from '../../images/debbyCovington.jpg'
+import debbyCovington from './debbyCovington.yaml'
 import thomasGulbransenImg from '../../images/thomasGulbransen.jpg'
 import thomasGulbransen from './thomasGulbransen.yaml'
 import angeliqueJohnsonImg from '../../images/angeliqueJohnson.png'
 import angeliqueJohnson from './angeliqueJohnson.yaml'
+import nasimKatebiImg from '../../images/nasimKatebi.jpg'
+import nasimKatebi from './nasimKatebi.yaml'
+import judyMonestimeImg from '../../images/judyMonestime.jpg'
+import judyMonestime from './judyMonestime.yaml'
 import jessOBrienImg from '../../images/jessOBrien.jpg'
 import jessOBrien from './jessOBrien.yaml'
 import danielaPinedaImg from '../../images/danielaPineda.jpg'
 import danielaPineda from './danielaPineda.yaml'
+import hongQinImg from '../../images/hongQin.jpg'
+import hongQin from './hongQin.yaml'
 import platoSmithImg from '../../images/platoSmith.jpg'
 import platoSmith from './platoSmith.yaml'
 import ashokSrinivasanImg from '../../images/ashokSrinivasan.jpg'
 import ashokSrinivasan from './ashokSrinivasan.yaml'
+import deborahSunterImg from '../../images/deborahSunter.jpg'
+import deborahSunter from './deborahSunter.yaml'
 
 export const speakers = [
+  {
+    image: debbyCovingtonImg,
+    ...debbyCovington
+  },
   {
     image: donnaEnnisImg,
     ...donnaEnnis
@@ -41,8 +55,16 @@ export const speakers = [
     ...angeliqueJohnson
   },
   {
+    image: nasimKatebiImg,
+    ...nasimKatebi
+  },
+  {
     image: yolandaLawsonImg,
     ...yolandaLawson
+  },
+  {
+    image: judyMonestimeImg,
+    ...judyMonestime
   },
   {
     image: jessOBrienImg,
@@ -53,12 +75,20 @@ export const speakers = [
     ...danielaPineda
   },
   {
+    image: hongQinImg,
+    ...hongQin
+  },
+  {
     image: platoSmithImg,
     ...platoSmith
   },
   {
     image: ashokSrinivasanImg,
     ...ashokSrinivasan
+  },
+  {
+    image: deborahSunterImg,
+    ...deborahSunter
   }
 ]
 
